042d9ccda3a7d1160af5b5f1f00ef6257d0023e8
danielogbuti <danielogbuti@gmail.com>
2022-11-03 11:09:46 +0100

    contract additions and optimizations

--- src/contracts/rider_contract.py
+++ src/contracts/rider_contract.py
@@ -2,24 +2,26 @@ from pyteal import *
 
 
 class Car:
+    admin = Addr(".//")
+
     class Variables:
         brand = Bytes("NAME")
         image = Bytes("IMAGE")
         description = Bytes("DESCRIPTION")
         location = Bytes("LOCATION")
         price = Bytes("PRICE")
-        available = Bytes("AVAILABLE")
+        quantity = Bytes("quantity")
+        approved = Bytes("approved")
         sold = Bytes("Sold")
-       
-        
 
     class AppMethods:
         buy = Bytes("buy")
         addmorecars = Bytes("addmorecars")
         changelocation = Bytes("changelocation")
-       
+        approve = Bytes("approve")
 
-    #new car listing
+    # new car listing
+
     def application_creation(self):
         return Seq([
             Assert(
@@ -29,70 +31,88 @@ class Car:
                     Len(Txn.application_args[0]) > Int(0),
                     Len(Txn.application_args[1]) > Int(0),
                     Len(Txn.application_args[2]) > Int(0),
-                     Len(Txn.application_args[3]) > Int(0),
+                    Len(Txn.application_args[3]) > Int(0),
                     Btoi(Txn.application_args[4]) > Int(0),
                     Len(Txn.application_args[5]) > Int(0),
                 )
-            ), 
+            ),
 
-          
+
             App.globalPut(self.Variables.brand, Txn.application_args[0]),
             App.globalPut(self.Variables.image, Txn.application_args[1]),
             App.globalPut(self.Variables.description, Txn.application_args[2]),
-             App.globalPut(self.Variables.location, Txn.application_args[3]),
+            App.globalPut(self.Variables.location, Txn.application_args[3]),
             App.globalPut(self.Variables.price, Btoi(Txn.application_args[4])),
-            App.globalPut(self.Variables.available, Txn.application_args[5]),
+            App.globalPut(self.Variables.quantity,
+                          Btoi(Txn.application_args[5])),
+            App.globalPut(self.Variables.approved, Int(0)),
             App.globalPut(self.Variables.sold, Int(0)),
 
             Approve(),
         ])
 
-  #buy car
+  # buy car
     def buy(self):
-            count = Txn.application_args[1]
-            valid_number_of_transactions = Global.group_size() == Int(2)
+        valid_number_of_transactions = Global.group_size() == Int(2)
 
-            valid_payment_to_seller = And(
-                Gtxn[1].type_enum() == TxnType.Payment,
-                Gtxn[1].receiver() == Global.creator_address(),
-                Gtxn[1].amount() == App.globalGet(self.Variables.price),
-                Gtxn[1].sender() == Gtxn[0].sender(),
-            )
+        valid_payment_to_seller = And(
+            Gtxn[1].type_enum() == TxnType.Payment,
+            Gtxn[1].receiver() == Global.creator_address(),
+            Gtxn[1].amount() == App.globalGet(self.Variables.price),
+            Gtxn[1].sender() == Gtxn[0].sender(),
+            App.globalGet(self.Variables.approved) != Int(0)
+        )
 
-            can_buy = And(valid_number_of_transactions,
-                        valid_payment_to_seller)
+        can_buy = And(valid_number_of_transactions,
+                      valid_payment_to_seller)
 
-            update_state = Seq([
-                App.globalPut(self.Variables.sold, App.globalGet(self.Variables.sold) + Int(1)),
-                App.globalPut(self.Variables.available, App.globalGet(self.Variables.available) - Int(1)),
-                Approve()
-            ])
-
-            return If(can_buy).Then(update_state).Else(Reject())
-
-    
-  # add more car to the listing
-    def addmorecars(self):
-        Assert(
-            And(
-                    Txn.sender() == Global.creator_address(),
-                    Txn.applications.length() == Int(1),
-                    Txn.application_args.length() == Int(2),
-            ),
-        ),
-        return Seq([
-            App.globalPut(self.Variables.available, Txn.application_args[2]),
+        update_state = Seq([
+            App.globalPut(self.Variables.sold, App.globalGet(
+                self.Variables.sold) + Int(1)),
+            App.globalPut(self.Variables.quantity, App.globalGet(
+                self.Variables.quantity) - Int(1)),
             Approve()
         ])
 
+        return If(can_buy).Then(update_state).Else(Reject())
 
-    #change location
+  # add more car to the listing
+
+    def addmorecars(self):
+        Assert(
+            And(
+                Txn.sender() == Global.creator_address(),
+                Txn.applications.length() == Int(1),
+                Txn.application_args.length() == Int(2),
+            ),
+        ),
+        return Seq([
+            App.globalPut(self.Variables.quantity,
+                          Btoi(Txn.application_args[2])),
+            Approve()
+        ])
+
+    def approve(self):
+        Assert(
+            And(
+                Txn.sender() == self.admin(),
+                Txn.applications.length() == Int(1),
+                Txn.application_args.length() == Int(2),
+            ),
+        ),
+        return Seq([
+            App.globalPut(self.Variables.approved,
+                          Int(1)),
+            Approve()
+        ])
+
+    # change location
     def changelocation(self):
         Assert(
             And(
-                    Txn.sender() == Global.creator_address(),
-                    Txn.applications.length() == Int(1),
-                    Txn.application_args.length() == Int(2),
+                Txn.sender() == Global.creator_address(),
+                Txn.applications.length() == Int(1),
+                Txn.application_args.length() == Int(2),
             ),
         ),
         return Seq([
@@ -100,12 +120,11 @@ class Car:
             Approve()
         ])
 
+    # delete car
 
-    #delete car
     def application_deletion(self):
         return Return(Txn.sender() == Global.creator_address())
 
-   
     def application_start(self):
         return Cond(
             # checks if the application_id field of a transaction matches 0.
@@ -116,7 +135,10 @@ class Car:
              self.application_deletion()],
             [Txn.application_args[0] == self.AppMethods.buy, self.buy()],
             [Txn.application_args[0] == self.AppMethods.addmorecars, self.addmorecars()],
-            [Txn.application_args[0] == self.AppMethods.changelocation, self.changelocation()],
+            [Txn.application_args[0] ==
+                self.AppMethods.changelocation, self.changelocation()],
+            [Txn.application_args[0] ==
+                self.AppMethods.approve, self.approve()],
         )
 
     # The approval program is responsible for processing all application calls to the contract.
